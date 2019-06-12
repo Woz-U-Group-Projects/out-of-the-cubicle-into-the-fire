fffc60004eccc600cd3fb0e084b0934279ad058a
Ammon King <ammon.king@woz-u.com>
2019-06-12 16:36:37 -0700

    ammon's stock project

--- client/README.md
+++ client/README.md
@@ -1,6 +1,6 @@
-# SpeedyProject
+# Client
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
 ## Development server
 
---
+++ src/main/java/com/group/cubiclefire/CubiclefireApplication.java
@@ -0,0 +1,13 @@
+package com.group.cubiclefire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubiclefireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubiclefireApplication.class, args);
+	}
+
+}
