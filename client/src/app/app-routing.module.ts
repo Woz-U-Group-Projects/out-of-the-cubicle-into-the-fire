f1ca69f2a2f61a8b8ed513af0ba4788f6e484b0f
Seymour James <seymour.37.sj@gmail.com>
2019-07-14 21:09:32 -0700

    bug fix.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-import { GetRequestComponent } from './fetch.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent}
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "profile", component: ProfileComponent }
 ];
 
 @NgModule({
-  declarations: [GetRequestComponent],
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/fetch.component.ts
+++ client/src/app/fetch.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 @Component({
-    selector: 'fetch-request'
+  selector: "fetch-request",
+  template: ""
 })
 export class GetRequestComponent {
-    apiData;
+  apiData;
 
-    getApi() {
-        const url = 'http://localhost:8080/api/values';
-        fetch(url)
-            .then(resp => resp.json())
-            .then(resp => (this.apiData = resp));
-    }
+  getApi() {
+    const url = "http://localhost:8080/api/values";
+    fetch(url)
+      .then(resp => resp.json())
+      .then(resp => (this.apiData = resp));
+  }
 }
