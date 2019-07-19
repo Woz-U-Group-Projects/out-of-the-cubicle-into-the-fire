1a6c969304d905d5e9ab785f168f51718ef814f1
Ryan <ryan.cawley@outlook.com>
2019-07-18 21:38:39 -0700

    /register

--- src/main/java/com/group/cubiclefire/UserController.java
+++ src/main/java/com/group/cubiclefire/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/register")
 public class UserController {
 
     private UserRepository userRepository;
